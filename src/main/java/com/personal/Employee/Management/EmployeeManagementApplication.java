07fa056b999e37096bda859daab572faaf3eed10
Mayuresh <mayuresh483@gmail.com>
2023-08-10 14:07:00 +0530

    Added CORS Filter to allow Angular Application to use REST Services

--- src/main/java/com/personal/Employee/Management/EmployeeManagementApplication.java
+++ src/main/java/com/personal/Employee/Management/EmployeeManagementApplication.java
@@ -2,12 +2,34 @@ package com.personal.Employee.Management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		config.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
+				"Accept","Authorization","Origin, Accept","X-Request-With",
+				"Access-Control-Request-Method","Access-Control-Request-Headers"));
+		config.setExposedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
+				"Accept","Authorization","Access-Control-Allow-Credentials"));
+		config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
